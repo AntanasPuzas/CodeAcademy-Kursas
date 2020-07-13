225c9c6836ee36f39002ba641a0735a6eb53bcb4
unknown <antanaspuzas@protonmail.com>
2020-07-13 16:31:44 +0300

    Task Complete

---
+++ Exception Tasks/Task_3/src/Exceptions/DeviceIsOffException.java
@@ -0,0 +1,17 @@
+package Exceptions;
+
+public class DeviceIsOffException extends Exception{
+    private String exception;
+
+    public DeviceIsOffException(String exception) {
+        this.exception = exception;
+    }
+
+    public String getException() {
+        return exception;
+    }
+
+    public void setException(String exception) {
+        this.exception = exception;
+    }
+}
